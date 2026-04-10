28f4015ca530417a7edf442a32ff8eef6541f3ab
Your Name <macielbrayn@gmail.com>
2026-04-10 15:32:38 -0600

    GitGnore

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cecytem.asistencias',
+  appName: 'Cecytem Asistencias',
+  webDir: 'dist/cecytem-admin-asistencias/browser'
+};
+
+export default config;
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBsnfc4bJfe7ADS-uKi57bD4teCrsGe_HY",
+    authDomain: "cecytemasistencias.firebaseapp.com",
+    projectId: "cecytemasistencias",
+    storageBucket: "cecytemasistencias.firebasestorage.app",
+    messagingSenderId: "1088424745267",
+    appId: "1:1088424745267:web:e79d0f5b3c1f539580c646"
+  }
+};
